d60a79a989607de168148670483ffd11a368a504
Maxim <maxim.orient@mail.ru>
2021-01-18 21:22:34 +1000

    Second18012021

--- Задание1.1.js
+++ Задание1.1.js
@@ -66,3 +66,12 @@ if(!hidden) {
 }
 
 showMyDB(personalMovieDB.privat);
+
+function writeYourGeners () {
+
+  for(let i = 1; i<=3 ;i++){
+    personalMovieDB.genres[i - 1] = prompt(`Ваш любымый жанр под номером ${i}`);
+  }
+}
+
+writeYourGeners ();

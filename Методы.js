ad18e5c572f27c187cb8d49f5ca3ec9051c2af3a
Maxim <maxim.orient@mail.ru>
2021-01-18 20:32:10 +1000

    Metody

--- Методы.js
+++ Методы.js
@@ -2,7 +2,29 @@
 
 const str = "test";
 
-console.log(str.length);
+// console.log(str[2]);
 
-console.log();
+// console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
 
+let fruit = 'Some fruit';
+
+console.log(fruit.indexOf('fruit'));
+
+const logg = "Hello world";
+
+// console.log(logg.slice(-5));
+
+console.log(logg.substring(6,11));
+
+console.log(logg.substr(6,5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+
+// console.log(parseInt(test));
+
+console.log(parseFloat(test));

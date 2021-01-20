6d798104c422717a92a681dd4646cbca8f428a33
Maxim <maxim.orient@mail.ru>
2021-01-20 16:19:17 +1000

    3

--- Динамическая типизация.js
+++ Динамическая типизация.js
@@ -2,13 +2,45 @@
 
 const box = document.getElementById('box'),
 	  btns = document.getElementsByTagName('button'),
-	  circles = document.getElementsByClassName('cirvle'),
-	  hearts = document.querySelectorAll('heart'),
+	  circles = document.getElementsByClassName('circle'),
+	  hearts = document.querySelectorAll('.heart'),
 	  one = document.querySelector('.heart');
 
-	  box.style.backgroundColor = 'blue';
-	  box.style.width = '500px';
+	//   box.style.backgroundColor = 'blue';
+	//   box.style.width = '500px';
 	 
+	  box.style.cssText = 'background-color:blue;width:500px';
 
 	  btns[1].style.borderRadius = '100%';
-	  circles[1].style.backgroundColor = 'red';
+	  circles[0].style.backgroundColor = 'red';
+
+	//   for ( let i = 0; i < hearts.length; i++){
+	// 	hearts[i].style.backgroundColor = "blue";
+	//   }
+
+	hearts.forEach(item => {
+
+		item.style.backgroundColor = 'blue'
+	});
+
+const div = document.createElement('div');
+// const text = document.createTextNode('Тут был я');
+
+div.classList.add('black');
+
+do
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

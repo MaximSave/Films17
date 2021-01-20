92bcf26080c8b5a1ff3445403c891215084aa1dd
Maxim <maxim.orient@mail.ru>
2021-01-20 15:30:21 +1000

    Динамическая типи

---
+++ Динамическая типизация.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const box = document.getElementById('box'),
+	  btns = document.getElementsByTagName('button'),
+	  circles = document.getElementsByClassName('cirvle'),
+	  hearts = document.querySelectorAll('heart'),
+	  one = document.querySelector('.heart');
+
+	  console.dir(box);
